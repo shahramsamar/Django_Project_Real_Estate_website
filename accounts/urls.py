7ef4c5d003fb9c85f33d326b846b053298fd1e3e
shahramsamar <shahramsamar2010@gmail.com>
2024-06-06 13:40:36 +0330

    added html pages  signup and signin

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,19 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from accounts.views import login_view, register_view
 
+
+ 
+# app_name ='accounts'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
-    path('accounts', include('accounts.urls')),
+    path('login', login_view, name='login'),
+    path('register', register_view, name='register'),
+
 ]
-
-
-# static
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
