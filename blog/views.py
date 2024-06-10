dd14792198d6523ca360dc9013c92e7afd1b4150
shahramsamar <shahramsamar2010@gmail.com>
2024-06-10 21:18:37 +0330

    added blog and blog_single

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def blog_view(request):
+    return render(request,'blog/blog.html')
+
+
+
+def blog_single_view(request):
+    return render(request, 'blog/blog_single.html')
