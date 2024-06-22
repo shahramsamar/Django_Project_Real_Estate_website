7169b11ab09d4269deef7d9bf0bb20e1fe049c50
shahramsamar <shahramsamar2010@gmail.com>
2024-06-22 09:40:14 +0330

    added  inclusion_tag for post_category in sidebar
    added post _category in blog_single and blog_home

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,15 +8,7 @@ register = template.Library()
 
 
 
-@register.inclusion_tag('blog/blog_post_category.html')
-def post_category():
-    posts = Post.objects.filter(status=1)
-    categories = Category.objects.all()
-    cat_dict = {}
-    for name in categories:
-        cat_dict[name] = posts.filter(category=name).count()
-    return {'categories': cat_dict}  
- 
+
 @register.simple_tag(name='counted_comments')
 def function(pid):
     post = Post.objects.filter(pk=pid)
@@ -32,7 +24,22 @@ def function():
     posts = Post.objects.filter(status=1)
     return posts
 
+
+
+
+
 @register.inclusion_tag('blog/blog_popular_post_widget.html')
 def popular_post_widget():
     posts = Post.objects.filter(status=1).order_by("published_date")[:3]
     return {"posts":posts}
+
+
+@register.inclusion_tag('blog/blog_post_category.html')
+def blog_post_category():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories': cat_dict}  
+ 
