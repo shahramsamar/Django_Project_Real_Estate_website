2fbcb354c7ed00759381702636aecd1c061e7a02
shahram <shahramsamar2010@gmail.com>
2025-07-20 16:24:42 +0330

    Update prod.py

--- core/setting/prod.py
+++ core/setting/prod.py
@@ -34,18 +34,13 @@ ALLOWED_HOSTS = ['*']
 #         }
 #     }
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+
 DATABASES = {
         'default': {
             'ENGINE': config("DB_ENGINE",default='django.db.backends.postgresql'),
-            'NAME': config("DB_NAME",default='project'),
+            'NAME': config("DB_NAME",default='asus'),
             'USER': config("DB_USER",default='postgres'),
-            'PASSWORD':config("DB_PASSWORD",default='0000'),
+            'PASSWORD':config("DB_PASSWORD",default='123'),
             'HOST': config("DB_HOST",default='127.0.0.1'),
             'PORT': config("DB_PORT",cast=int,default='5432'),
         }
